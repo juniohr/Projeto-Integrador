4a085559db5d7bda122e835207975ad991684221
juniohr <rockstararcano@gmail.com>
2020-09-13 20:46:14 -0300

    Acrescimos de itens coletáveis

---
+++ Projeto Integrador/Assets/Scripts/Orange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orange : MonoBehaviour
+{
+    public GameObject obj;
+    public SpriteRenderer spriteR;
+    public CircleCollider2D cirCol;
+
+    private void Start()
+    {
+        obj.SetActive(false);
+        spriteR = GetComponent<SpriteRenderer>();
+        cirCol = GetComponent<CircleCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            spriteR.enabled = false;
+            cirCol.enabled = false;
+            obj.SetActive (true);
+            Destroy(gameObject, 0.3f);
+        }
+    }
+}
