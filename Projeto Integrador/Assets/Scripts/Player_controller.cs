336da84abcbbe63da384f2a1b13cba0ae2bcd17c
juniohr <rockstararcano@gmail.com>
2020-09-15 02:17:46 -0300

    Mecânicas de Game Over e sistema de coleta com UI

---
+++ Projeto Integrador/Assets/Scripts/Player_controller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Player_controller : MonoBehaviour
+{
+    public Text txt;
+    public int totalScore = 0;
+    public static Player_controller instance;
+    public GameObject gameOver;
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void Score()
+    {
+        txt.text = totalScore.ToString();
+    }
+
+    public void Gameover()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+}
